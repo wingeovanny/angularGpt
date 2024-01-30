81c7d8390db5a2ca1a7278839fdaab9336c54f8e
Geo Bucay <wingeovanny88@gmail.com>
2024-01-29 22:46:57 -0500

    burbujas de chat

--- src/app/presentation/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/presentation/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,12 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SidebarMenuItemComponent } from '../../components/sidebar-menu-item/sidebar-menu-item.component';
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SidebarMenuItemComponent],
   templateUrl: './dashboard-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardLayoutComponent {}
+export class DashboardLayoutComponent {
+  public routes = routes[0].children?.filter((route) => route.data);
+}
--- src/app/presentation/pages/orthography-page/orthography-page.component.ts
+++ src/app/presentation/pages/orthography-page/orthography-page.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChatMessageComponent } from '../../components/chat-bubbles/chat-message/chat-message.component';
+import { MyMessageComponent } from '../../components/chat-bubbles/my-message/my-message.component';
 
 @Component({
   selector: 'app-orthography-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ChatMessageComponent, MyMessageComponent],
   templateUrl: './orthography-page.component.html',
   styleUrl: './orthography-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
